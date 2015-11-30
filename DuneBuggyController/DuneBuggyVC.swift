d95e454c450480b90f9c3d3b95031cc88a27b90a
rnorback <rnorback@gmail.com>
2015-11-29 18:01:55 -0700

    The phone communicates with the car and turns it on

--- DuneBuggyController/DuneBuggyVC.swift
+++ DuneBuggyController/DuneBuggyVC.swift
@@ -53,6 +53,8 @@ class DuneBuggyVC: UIViewController, CBCentralManagerDelegate, CBPeripheralDeleg
                 sender.setValue(0, animated: true)
             }, completion: nil)
         
+        
+        
         if sender == leftSlider {
             leftLabel.text = "0"
         } else if sender == rightSlider {
@@ -86,22 +88,91 @@ class DuneBuggyVC: UIViewController, CBCentralManagerDelegate, CBPeripheralDeleg
     //MARK: CBCentralManagerDelegate
     
     func centralManagerDidUpdateState(central: CBCentralManager) {
-        var state:String
+        var state:String = ""
+        var showAlert:Bool = true
+        
         switch central.state {
+        case .Unsupported:
+            state = "This device does not support Bluetooth Low Energy."
+        case .Unauthorized:
+            state = "This app is not authorized to use Bluetooth Low Energy."
+        case .PoweredOff:
+            state = "Bluetooth on this device is currently powered off."
+        case .Resetting:
+            state = "The BLE Manager is resetting; a state update is pending."
         case .PoweredOn:
+            showAlert = false
+            state = "Bluetooth LE is turned on and ready for communication."
             keepScanning = true
             NSTimer.scheduledTimerWithTimeInterval(TIMER_SCAN_INTERVAL, target: self, selector: Selector("pauseScan"), userInfo: nil, repeats: false)
             centralManager.scanForPeripheralsWithServices([BlackWidowService], options: nil)
-        default:
-            state = "The state of the BLE Manager is unknown"
+        case .Unknown:
+            state = "The state of the BLE Manager is unknown."
+        }
+        
+        if showAlert {
+            let ac = UIAlertController(title: "Central Manager State", message: state, preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+            ac.addAction(okAction)
+            self.presentViewController(ac, animated: true, completion: nil)
         }
     }
     
     func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
-        keepScanning = false
-        blackWidow = peripheral
-        blackWidow.delegate = self
-        centralManager.connectPeripheral(blackWidow, options: nil)
+        if let name = peripheral.name {
+            if name.containsString("Black Widow") {
+                keepScanning = false
+                blackWidow = peripheral
+                blackWidow.delegate = self
+                centralManager.connectPeripheral(blackWidow, options: nil)
+            }
+        }
     }
     
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        connectionImageView.image = UIImage(named: "Bluetooth_Connected")
+        peripheral.discoverServices([BlackWidowService])
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        // Connection failed
+    }
+    
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        connectionImageView.image = UIImage(named: "Bluetooth_Disconnected")
+        keepScanning = true
+        self.resumeScan()
+    }
+    
+    //MARK: CBPeripheralDelegate
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        for service:CBService in peripheral.services! {
+            if service.UUID == BlackWidowService {
+                peripheral.discoverCharacteristics(nil, forService: service)
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        
+        for characteristic:CBCharacteristic in service.characteristics! {
+            if characteristic.UUID == BlackWidowCharRead {
+                blackWidow.setNotifyValue(true, forCharacteristic: characteristic)
+            }
+            
+            if characteristic.UUID == BlackWidowCharWrite {
+                print("Slider value: \(leftSlider.value)")
+                var motorValue:UInt8 = UInt8(leftSlider.value)
+                let motorBytes = NSData(bytes: &motorValue, length: sizeof(UInt8))
+                blackWidow.writeValue(motorBytes, forCharacteristic: characteristic, type: .WithResponse)
+            }
+        }
+        
+        
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        
+    }
 }
